f1d03308f0e5322760496afa23b3d67f8eb14a13
alx <alexsander.molina@compwire.com.br>
2022-10-13 13:43:45 -0300

    added script

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+// $(".terminal").click(function () {
+//    $('window[class^="window"]').css('z-index', '0');
+//    $(this).css('z-index', '10');
+// })
+// $(".overlay").click(function () {
+//    // make div 2 front of the screen
+//    $('window[class^="window"]').css('z-index', '0');
+//    $(this).css('z-index', '10');
+// })
+// $(".projects").click(function () {
+//    // make div 3 front of the screen
+//    $('window[class^="window"]').css('z-index', '0');
+//    $(this).css('z-index', '10');
+// })
